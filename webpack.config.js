f6919d3bd3a793f29a39da3d262efbf28285bdda
Hemanth <trhemanth2@gmail.com>
2021-10-06 22:07:50 +0530

    U: webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
-module.exports = {
-	name: 'sconfig',
-	target: 'node',
-	entry: '',
+const path = require('path');
+
+const clientConfig = {
+	name: 'web',
+	target: 'web',
+	entry: '.src/index.js',
 	output: {
-		path: __dirname,
-		filename: 'server.js',
-		libraryTarget: 'commonjs2',
+		path: path.resolve(__dirname, 'public/js'),
+		filename: 'bundle.js',
 		clean: true
 	},
 	module: {
 		rules: [
 			{
-				test: /\.js%/,
+				test: /\.js$/,
 				exclude: /(node_modules)/,
 				use: {
 					loader: 'babel-loader',
@@ -23,3 +24,30 @@ module.exports = {
 		]
 	}
 }
+
+const serverConfig = {
+	name: 'server',
+	target: 'node',
+	entry: './src/server.js',
+	output: {
+		path: __dirname,
+		filename: 'server.js',
+		libraryTarget: 'commonjs2'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/env', '@babel/react']
+					}
+				}
+			}
+		]
+	}
+}
+
+module.exports = [clientConfig, serverConfig];
